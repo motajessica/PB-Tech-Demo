dd703aa64bcd753a92f567a111455bf516e67eef
motajessica <motajessicaa@gmail.com>
2023-06-05 12:10:22 +1200

    Create laptop database

--- frontend/src/components/Body.js
+++ frontend/src/components/Body.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Routes, Route, Outlet} from 'react-router-dom';
 
 import Page0 from './Page0';
-import Page1 from './Page1';
+import LaptopResults from './LaptopResults';
 import Page2 from './Page2';
 
 function Body() {
@@ -11,7 +11,7 @@ function Body() {
     <div>
       <Routes>
         <Route exact path="/page0" element={<Page0/>} />
-        <Route path="/page1" element={<Page1/>} />
+        <Route path="/business_laptops" element={<LaptopResults/>} />
         <Route path="/page2" element={<Page2/>} />
       </Routes>
       <Outlet />
