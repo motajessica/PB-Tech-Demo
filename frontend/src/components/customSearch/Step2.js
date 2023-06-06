02b247e8fcc1ff812fb7b97a03bc6a45b62aa003
gregchang2 <greg.chang.is@gmail.com>
2023-06-07 09:04:44 +1200

    added step two check form basic structure (#7)

--- frontend/src/components/customSearch/Step2.js
+++ frontend/src/components/customSearch/Step2.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Formik, Form, Field } from 'formik';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Step2 = ({ formState, updateFormState }) => {
-
   const navigate = useNavigate();
 
+  const laptopBudgetOptions = [
+    "Quite Low",
+    "Low",
+    "Average",
+    "Above Average",
+    "Expensive",
+    "Premium",
+  ];
+  const laptopFeatureOptions = [
+    "HD",
+    "4K",
+    "FHD",
+    "Dedicated GPU",
+    "Touchscreen",
+    "SSD Storage",
+    "Fingerprint Security",
+    "Backlit Keyboard",
+    "VR ready",
+    "IPS display",
+    "Refurbished",
+  ];
+  const cpuOptions = [
+    "Low end Processors",
+    "Intel Core i3 Processors",
+    "Intel Core i5 Processors",
+    "Intel Core i7 Processors",
+    "Intel Core i9 Processors",
+    "AMD Ryzen 3 Processors",
+    "AMD Ryzen 5 Processors",
+    "AMD Ryzen 7 Processors",
+    "AMD Ryzen 9 Processors",
+    "Apple M1",
+    "Apple M2 Pro",
+  ];
   return (
     <div>
       <div className="custom-search-title fw-bold mt-5">
@@ -15,16 +48,65 @@ const Step2 = ({ formState, updateFormState }) => {
 
       <Formik
         initialValues={{
-          
+          laptopBudget: formState.laptopBudget || [],
+          laptopFeatures: formState.laptopFeatures || [],
+          cpuOptions: formState.cpuOptions || [],
         }}
         onSubmit={(values) => {
-          updateFormState({ ...formState, ...values});
-          // Trigger search submission here. To be implemented at the end
+          updateFormState({ ...formState, ...values });
+          navigate("/Page2");
         }}
       >
         <Form className="form">
-          {/* Implement fields here */}
-          <button type="submit">Find your perfect laptop</button>
+          <div className="form-group mb-3">
+            <span>What is your budget?</span>
+            <div className="form-check">
+              {laptopBudgetOptions.map((option) => (
+                <div key={option}>
+                  <Field
+                    type="checkbox"
+                    name="laptopBudget"
+                    value={option}
+                    className="form-check-input"
+                  />
+                  <label className="form-check-label">{option}</label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="form-group mb-3">
+            <span>Which Features do you want in your Laptop?</span>
+            <div className="form-check">
+              {laptopFeatureOptions.map((option) => (
+                <div key={option}>
+                  <Field
+                    type="checkbox"
+                    name="laptopFeatures"
+                    value={option}
+                    className="form-check-input"
+                  />
+                  <label className="form-check-label">{option}</label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="form-group mb-3">
+            <span>What type of processor are you looking for?</span>
+            <div className="form-check">
+              {cpuOptions.map((option) => (
+                <div key={option}>
+                  <Field
+                    type="checkbox"
+                    name="cpuOptions"
+                    value={option}
+                    className="form-check-input"
+                  />
+                  <label className="form-check-label">{option}</label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <button type="submit">Find your perfect Laptop</button>
         </Form>
       </Formik>
     </div>
