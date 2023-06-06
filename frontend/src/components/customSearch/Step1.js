c6d0ffa470e4a7132c0d08269e8f3eda78501d94
motajessica <motajessicaa@gmail.com>
2023-06-07 00:13:08 +1200

    Use Formik to make form state management easier

--- frontend/src/components/customSearch/Step1.js
+++ frontend/src/components/customSearch/Step1.js
@@ -1,91 +1,80 @@
-import React, {useState} from 'react'
-import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Step1 = ({ formState, updateFormState }) => {
-  const [category, setCategory] = useState("");
 
-  const handleCategoryChange = (event) => {
-    updateFormState({ ...formState, category})
-    setCategory(event.target.value);
-  };
-  
+  const navigate = useNavigate();
+
   const laptopTypesOptions = [
-    { label: "Traditional Laptop", value: "traditional" },
-    { label: "2 in 1 Laptop", value: "two_in_one" },
-    { label: "Chromebook", value: "chromebook" }
+    { label: 'Traditional Laptop', value: 'traditional' },
+    { label: '2 in 1 Laptop', value: 'two_in_one' },
+    { label: 'Chromebook', value: 'chromebook' },
   ];
 
   const screenSizeOptions = ['11-12', '13', '14', '15', '16', '17'];
-  const [selectedScreenSizeOptions, setSelectedScreeSizeOptions] = useState([])
 
-  const handlescreenSizeChange = (event) => {
-    const option = event.target.value;
-
-    if (event.target.checked) {
-      setSelectedScreeSizeOptions([...selectedScreenSizeOptions, option]);
-    } else {
-      setSelectedScreeSizeOptions(selectedScreenSizeOptions.filter((selectedOption) => selectedOption !== option));
-    }
-    updateFormState({ ...formState, screenSizes: selectedScreenSizeOptions})
-  }
-  return(
+  return (
     <div>
       <div className="custom-search-title fw-bold mt-5">
-        <h1 className="filter-border"> Find your perfefct laptop</h1>
+        <h1 className="filter-border"> Find your perfect laptop</h1>
       </div>
       <p>
-        With all different specs and extras added to Laptops, it can get
-        confusing when trying to decide which fits you best. Get started by
-        answering the questions below.
+        With all different specs and extras added to Laptops, it can get confusing when trying to decide which fits
+        you best. Get started by answering the questions below.
       </p>
-    
-      <form className="form">
-        <div className="form-group mb-3">
-          <span>What type of laptop are you looking for?</span>
-          <div>
-          {
-            laptopTypesOptions.map((option) => (
-              <div className="form-check-inline">
-                <label className="form-check-label" key={option.value}>
-                  <input
-                    type="radio"
-                    name="option"
-                    value={option.value}
-                    checked={category === option.value}
-                    onChange={handleCategoryChange}
-                    className="form-check-input me-1"
-                  />
-                  {option.label}
-                </label>
-              </div>
-            ))
-          }
-          </div>
-        </div>
-        <div className='form-group mb-3'>
-          <label>Screen size</label>
-           <div className='form-check'>
-            {screenSizeOptions.map((option) => {
-              return(
-                <div>
-                  <input
-                      type="checkbox"
-                      value={option}
-                      checked={selectedScreenSizeOptions.includes(option)}
-                      onChange={handlescreenSizeChange}
-                      className='form-check-input'
+
+      <Formik
+        initialValues={{
+          category: formState.category || '',
+          screenSizes: formState.screenSizes || [],
+        }}
+        onSubmit={(values) => {
+          updateFormState({ ...formState, ...values });
+          navigate('/custom_search/step2')
+        }}
+      >
+        <Form className="form">
+          <div className="form-group mb-3">
+            <span>What type of laptop are you looking for?</span>
+            <div>
+              {laptopTypesOptions.map((option) => (
+                <div className="form-check-inline" key={option.value}>
+                  <label className="form-check-label">
+                    <Field
+                      type="radio"
+                      name="category"
+                      value={option.value}
+                      className="form-check-input me-1"
                     />
-                    <label key={option} className="form-check-label">{option}</label>
+                    {option.label}
+                  </label>
                 </div>
-              )
-            })}
+              ))}
+            </div>
           </div>
-        </div>
-      </form>
-      <Link to="/step2">Next</Link>
+          <div className="form-group mb-3">
+            <label>Screen size</label>
+            <div className="form-check">
+              {screenSizeOptions.map((option) => (
+                <div key={option}>
+                  <Field
+                    type="checkbox"
+                    name="screenSizes"
+                    value={option}
+                    className="form-check-input"
+                  />
+                  <label className="form-check-label">{option}</label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <button type="submit">Next</button>
+        </Form>
+      </Formik>
     </div>
-  ) 
+  );
 };
 
 export default Step1;
