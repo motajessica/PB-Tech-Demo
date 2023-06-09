47bce54579e526b832f7af3205dfb072f6975ffb
Jessica Mota <114623028+motajessica@users.noreply.github.com>
2023-06-09 12:25:36 +1200

    Fix docker compose file from the backend (#15)

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 let dbUrl = `mongodb://mongo:27017/mongo`
+
 // dbUrl = `mongodb://localhost:27017/mongo` // Uncomment this to run on your local environment
 
 mongoose
