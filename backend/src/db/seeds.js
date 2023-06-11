df9a1f601a8fed871b12232340f3d41bb4ba25a0
motajessica <motajessicaa@gmail.com>
2023-06-12 11:15:49 +1200

    Test

--- backend/src/db/seeds.js
+++ backend/src/db/seeds.js
@@ -7,7 +7,7 @@ const laptopTypes = require("./LaptopTypes")
 const LaptopType = require("../models/laptopType")
 // let dbUrl = `mongodb://mongo:27017/mongo`
 
-// dbUrl = `mongodb://localhost:27017/mongo` // Uncomment this to run on your local environment
+// dbUrl = `mongodb://localhost:27017/mongo`  Uncomment this to run on your local environment
 
 mongoose.connect(dbUrl, { useNewUrlParser: true }).then(
   () => {
