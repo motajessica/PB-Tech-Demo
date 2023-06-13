6f7ca261cbe993206e843652e02b7efc794b63b6
motajessica <motajessicaa@gmail.com>
2023-06-14 08:59:15 +1200

    Change Radio Button

--- frontend/src/components/CustomRadioButton.js
+++ frontend/src/components/CustomRadioButton.js
@@ -1,22 +1,18 @@
 import React from 'react';
 
 const CustomRadioButton = ({ field, form, ...props }) => {
+  const handleClick = () => {
+    form.setFieldValue(field.name, props.value);
+  };
+
   return (
-    <div className="btn-group" role="group" aria-label={props.name}>
-      <input
-        type="radio"
-        className="btn-check"
-        name={props.name}
-        id={props.id}
-        autoComplete="off"
-        value={props.value}
-        checked={field.value === props.value}
-        onChange={field.onChange}
-      />
-      <label className={`btn ${props.checked ? 'btn-secondary active' : 'btn-secondary'}`} htmlFor={props.id}>
-        {props.label}
-      </label>
-    </div>
+    <button
+      type="button"
+      className={`btn ${field.value === props.value ? 'btn-primary active' : 'btn-secondary'}`}
+      onClick={handleClick}
+    >
+      {props.label}
+    </button>
   );
 };
 
--- frontend/src/components/customSearch/Step1.js
+++ frontend/src/components/customSearch/Step1.js
@@ -1,32 +1,40 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Formik, Form, Field } from "formik";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import {Breadcrumb, BreadcrumbItem} from "react-bootstrap";
-import CustomSearchHeader from "./CustomSearchHeader";
-import Sidebar from "../layout/SideBar";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { Breadcrumb, BreadcrumbItem } from 'react-bootstrap';
+import CustomSearchHeader from './CustomSearchHeader';
+import CustomRadioButton from '../CustomRadioButton';
 
 const Step1 = ({ formState, updateFormState }) => {
   const navigate = useNavigate();
 
   const laptopTypesOptions = [
-    { label: "Business laptop", value: "business" },
-    { label: "2 in 1 Laptop", value: "two_in_one" },
-    { label: "Chromebook", value: "chromebook" },
+    { label: 'Business laptop', value: 'business' },
+    { label: '2 in 1 Laptop', value: '2_in_1' },
+    { label: 'Chromebook', value: 'chromebook' },
   ];
 
-  const screenSizeOptions = ["11 inches", "12 inches", "13 inches", "14 inches", "15 inches", "16 inches", "17 inches"];
+  const screenSizeOptions = [
+    '11 inches',
+    '12 inches',
+    '13 inches',
+    '14 inches',
+    '15 inches',
+    '16 inches',
+    '17 inches',
+  ];
 
   const usageOptions = [
-    "General use",
-    "Home use",
-    "School use",
-    "Office work",
-    "Gaming",
-    "Heavy Work",
-    "Programming",
+    'General use',
+    'Home use',
+    'School use',
+    'Office work',
+    'Gaming',
+    'Heavy Work',
+    'Programming',
   ];
 
   return (
@@ -35,48 +43,44 @@ const Step1 = ({ formState, updateFormState }) => {
         <Breadcrumb>
           <BreadcrumbItem href="/">Home</BreadcrumbItem>
           <BreadcrumbItem href="/">Computers & Tablets</BreadcrumbItem>
-          <BreadcrumbItem href="/laptops_types">Laptops Types</BreadcrumbItem>
-          <BreadcrumbItem href="/custom_search/step1" active>Find your Perfect Laptop</BreadcrumbItem>
+          <BreadcrumbItem href="/laptops">Laptops</BreadcrumbItem>
+          <BreadcrumbItem href="/custom_search/step1" active>
+            Find your Perfect Laptop
+          </BreadcrumbItem>
         </Breadcrumb>
         <div className="row">
           <div className="col-12">
-            <CustomSearchHeader/>
+            <CustomSearchHeader />
             <p className="text-muted">
-              With all different specs and extras added to Laptops, it can get
-              confusing when trying to decide which fits you best. Get started by
-              answering the questions below.
+              With all different specs and extras added to Laptops, it can get confusing when trying to decide which
+              fits you best. Get started by answering the questions below.
             </p>
 
             <Formik
               initialValues={{
-                laptopType: formState.laptopType || "",
+                laptopType: formState.laptopType || '',
                 screenSize: formState.screenSize || [],
                 purposes: formState.purposes || [],
               }}
               onSubmit={(values) => {
                 updateFormState({ ...formState, ...values });
-                navigate("/custom_search/step2");
+                navigate('/custom_search/step2');
               }}
             >
               <Form className="form">
                 <div className="form-group mb-3 mt-5">
-            <span className="filter-category-title">
-              What type of laptop are you looking for?
-            </span>
+                  <span className="filter-category-title">What type of laptop are you looking for?</span>
                   <div>
                     {laptopTypesOptions.map((option) => (
-                          <div className="form-check-inline" key={option.value}>
-                            <label className="form-check-label mt-2">
-                              <Field
-                                type="radio"
-                                name="laptopType"
-                                value={option.value}
-                                className="form-check-input me-1"
-                              />
-                              {option.label}
-                            </label>
-                          </div>
-                        ))}
+                      <Field
+                        key={option.value}
+                        component={CustomRadioButton}
+                        name="laptopType"
+                        id={`laptopType_${option.value}`}
+                        value={option.value}
+                        label={option.label}
+                      />
+                    ))}
                   </div>
                 </div>
 
@@ -91,16 +95,14 @@ const Step1 = ({ formState, updateFormState }) => {
                           value={option}
                           className="form-check-input"
                         />
-                        <label className="form-check-label mb-3 ">{option}</label>
+                        <label className="form-check-label mb-3">{option}</label>
                       </div>
                     ))}
                   </div>
                 </div>
 
                 <div className="form-group mb-3">
-                  <label className="filter-category-title">
-                    What kind of work will you do with this laptop?
-                  </label>
+                  <label className="filter-category-title">What kind of work will you do with this laptop?</label>
                   <div className="form-check mt-3">
                     {usageOptions.map((option) => (
                       <div key={option}>
@@ -127,9 +129,9 @@ const Step1 = ({ formState, updateFormState }) => {
                 </div>
               </Form>
             </Formik>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 };
