76b7d92be9c8023b53d8bf8fab7da14280acde8c
motajessica <motajessicaa@gmail.com>
2023-06-14 16:30:00 +1200

    WIP - Fix checkbox for the first filter on step 2

--- frontend/src/components/customSearch/Step2.js
+++ frontend/src/components/customSearch/Step2.js
@@ -13,12 +13,12 @@ const Step2 = ({ formState, updateFormState }) => {
   const navigate = useNavigate();
 
   const laptopBudgetOptions = [
-    { label: 'Quite Low', value: 1725 },
-    { label: "Low", value: 1725},
-    { label: "Average", value: 1725},
-    { label: "Above Average", value:1725},
-    { label: "Expensive", value: 1725},
-    { label: "Premium", value: 1725},
+    { label: "Quite Low", value: 1725 },
+    { label: "Low", value: 1725 },
+    { label: "Average", value: 1725 },
+    { label: "Above Average", value: 1725 },
+    { label: "Expensive", value: 1725 },
+    { label: "Premium", value: 1725 },
   ];
 
   // const laptopBudgetOptions = [
@@ -58,98 +58,105 @@ const Step2 = ({ formState, updateFormState }) => {
   ];
   return (
     <div>
-    <div className="p-4">
-      <Breadcrumb>
-        <BreadcrumbItem href="/">Home</BreadcrumbItem>
-        <BreadcrumbItem href="/">Computers & Tablets</BreadcrumbItem>
-        <BreadcrumbItem href="/laptops_types">Laptops Types</BreadcrumbItem>
-        <BreadcrumbItem href="/custom_search/step1" active>Find your Perfect Laptop</BreadcrumbItem>
-      </Breadcrumb>
-      <div className="row">
-        <div className="col-12">
-          <CustomSearchHeader/>
+      <div className="p-4">
+        <Breadcrumb>
+          <BreadcrumbItem href="/">Home</BreadcrumbItem>
+          <BreadcrumbItem href="/">Computers & Tablets</BreadcrumbItem>
+          <BreadcrumbItem href="/laptops_types">Laptops Types</BreadcrumbItem>
+          <BreadcrumbItem href="/custom_search/step1" active>
+            Find your Perfect Laptop
+          </BreadcrumbItem>
+        </Breadcrumb>
+        <div className="row">
+          <div className="col-12">
+            <CustomSearchHeader />
+          </div>
+
+          <Formik
+            initialValues={{
+              laptopBudget: formState.laptopBudget || [],
+              laptopFeatures: formState.laptopFeatures || [],
+              gpuOptions: formState.gpuOptions || [],
+            }}
+            onSubmit={(values) => {
+              updateFormState({ ...formState, ...values });
+              const search = `?${createSearchParams({
+                ...formState,
+                ...values,
+              })}`;
+              navigate({ pathname: "/page2", search });
+            }}
+          >
+            <Form className="form">
+              <div className="form-group mb-3 mt-5">
+                <span className="filter-category-title">
+                  What is your budget?
+                </span>
+                <div className="form-check mt-3">
+                  {laptopBudgetOptions.map((option) => (
+                    <div key={option.value}>
+                      <Field
+                        type="checkbox"
+                        name="laptopBudget" // Updated name attribute
+                        value={option.label}
+                        className="form-check-input"
+                      />
+                      <label className="form-check-label mb-3">
+                        {option.label}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="form-group mb-3">
+                <span className="filter-category-title">
+                  Which Features do you want in your Laptop?
+                </span>
+                <div className="form-check mt-3">
+                  {laptopFeatureOptions.map((option) => (
+                    <div key={option}>
+                      <Field
+                        type="checkbox"
+                        name="laptopFeatures"
+                        value={option}
+                        className="form-check-input"
+                      />
+                      <label className="form-check-label mb-3">{option}</label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="form-group mb-3">
+                <span className="filter-category-title">
+                  What type of processor are you looking for?
+                </span>
+                <div className="form-check mt-3">
+                  {gpuOptions.map((option) => (
+                    <div key={option}>
+                      <Field
+                        type="checkbox"
+                        name="gpuModel"
+                        value={option}
+                        className="form-check-input"
+                      />
+                      <label className="form-check-label mb-3 ">{option}</label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="d-flex justify-content-end align-items-center">
+                <button
+                  type="submit"
+                  className="filter-next-back-border px-5 d-flex align-items-center"
+                >
+                  Find your perfect Laptop
+                  <FontAwesomeIcon className="ms-3" icon={faArrowRight} />
+                </button>
+              </div>
+            </Form>
+          </Formik>
         </div>
-
-      <Formik
-        initialValues={{
-          laptopBudget: formState.laptopBudget || [],
-          laptopFeatures: formState.laptopFeatures || [],
-          gpuOptions: formState.gpuOptions || [],
-        }}
-
-        onSubmit={(values) => {
-          updateFormState({ ...formState, ...values });
-          const search = `?${createSearchParams({ ...formState, ...values })}`;
-          navigate({ pathname: "/page2", search });
-        }}
-      >
-        <Form className="form">
-          <div className="form-group mb-3 mt-5">
-            <span className="filter-category-title">What is your budget?</span>
-            <div className="form-check mt-3">
-              {laptopBudgetOptions.map((option) => (
-                <div key={option}>
-                  <Field
-                    type="checkbox"
-                    name="price"
-                    value={option.value}
-                    id={`price${option.value}`}
-                    className="form-check-input"
-                  />
-                  <label className="form-check-label mb-3">{option.label}</label>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="form-group mb-3">
-            <span className="filter-category-title">
-              Which Features do you want in your Laptop?
-            </span>
-            <div className="form-check mt-3">
-              {laptopFeatureOptions.map((option) => (
-                <div key={option}>
-                  <Field
-                    type="checkbox"
-                    name="laptopFeatures"
-                    value={option}
-                    className="form-check-input"
-                  />
-                  <label className="form-check-label mb-3">{option}</label>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="form-group mb-3">
-            <span className="filter-category-title">
-              What type of processor are you looking for?
-            </span>
-            <div className="form-check mt-3">
-              {gpuOptions.map((option) => (
-                <div key={option}>
-                  <Field
-                    type="checkbox"
-                    name="gpuModel"
-                    value={option}
-                    className="form-check-input"
-                  />
-                  <label className="form-check-label mb-3 ">{option}</label>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className="d-flex justify-content-end align-items-center">
-            <button
-              type="submit"
-              className="filter-next-back-border px-5 d-flex align-items-center"
-            >
-              Find your perfect Laptop
-              <FontAwesomeIcon className="ms-3" icon={faArrowRight} />
-            </button>
-          </div>
-        </Form>
-      </Formik>
-    </div>
-    </div>
+      </div>
     </div>
   );
 };
